beaa224add9cc74ce526cc400463870ece65e238
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2023-02-25 16:20:37 -0500

    added basic logo square

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+
+const Logo = () => {
+    return (
+        <Tilt className="Tilt br2 shadow-2">
+            <div className="ma4 mt0 logo">
+                Logo
+            </div>
+        </Tilt>
+    )
+}
+
+export default Logo
