690e1d67fc195856061780c62ab54c8899a930e1
MatthewIsfeld <57026982+MatthewIsfeld@users.noreply.github.com>
2023-02-26 14:16:52 -0500

    Added base component to show a user their rank, and added a particles bacground

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navigation from "./components/Navigation/Navigation"
 import Logo from "./components/Logo/Logo"
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div>
+      <ParticlesBg className="particles" bg={true}></ParticlesBg>
       <Navigation></Navigation>
       <Logo></Logo>
+      <Rank></Rank>
       <ImageLinkForm></ImageLinkForm>
     </div>
   );
---
+++ client/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='center white f3'>
+                {"Andrei Your Current Rank is:"}
+            </div>
+            <div className='center white f1'>
+                {"#5"}
+            </div>
+        </div>
+    )
+}
+
+export default Rank
